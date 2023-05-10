bc19cb5fbbcf3f530d83ca2b82c74d135f9e6c81
ayadav <123587824+ayadav313@users.noreply.github.com>
2023-05-10 17:52:03 -0400

    feat(WorkoutTracker.js): add Exercise component to display exercise data and input fields for sets and reps. Add useEffect hook to fetch exercise data from server.

--- client/src/components/workoutTracker/WorkoutTracker.js
+++ client/src/components/workoutTracker/WorkoutTracker.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import Exercise from '../common/Exercise';
 
 const WorkoutTracker = () => {
   const location = useLocation();
@@ -8,11 +9,30 @@ const WorkoutTracker = () => {
   const [workout, setWorkout] = useState(null);
   const navigate = useNavigate();
 
+  const [exercises, setExercises] = useState({});
+
   useEffect(() => {
     if (workoutData) {
       setWorkout(workoutData);
     }
   }, [workoutData]);
+
+  useEffect(() => {
+    const fetchExercises = async () => {
+      const exerciseData = {};
+      for (const exerciseLog of workout.exerciseLogs) {
+        const response = await fetch(`http://localhost:3000/exercises/${exerciseLog.exerciseId}`);
+        const data = await response.json();
+        exerciseData[exerciseLog.exerciseId] = data;
+      }
+      setExercises(exerciseData);
+    };
+  
+    if (workout) {
+      fetchExercises();
+    }
+  }, [workout]);
+  
 
   const [exerciseLogs, setExerciseLogs] = useState([]);
 
@@ -23,8 +43,9 @@ const WorkoutTracker = () => {
     ]);
   };
 
+
+
   const handleSubmit = () => {
-    // TODO: handleSubmit()
     console.log('Workout saved successfully!', { workoutId: workoutData._id, exerciseLogs });
     alert('Workout saved successfully!');
     navigate('/dashboard');
@@ -33,25 +54,37 @@ const WorkoutTracker = () => {
   if (!workout) {
     return <div>Loading...</div>;
   }
-  console.log(workout);
 
+  console.log(workout.exerciseLogs);
   return (
     <div className="workout-tracker container">
       <h3 className="my-4">{workout.name}</h3>
-      {workout.exerciseLogs.map((exercise, idx) => (
-        <div key={idx} className="exercise-log card mb-3 p-3">
-          <h4 className="card-title">{exercise.name}</h4>
+      {workout.exerciseLogs.map((exercise, idx) => {
+  const exerciseData = exercises[exercise.exerciseId];
+  return (
+    <div key={idx} className="exercise-log card mb-3 p-3">
+      {exerciseData ? (
+        <>
+        {/* TODO INSTEAD OF THESE 3 LINES UNDERNEATH JUST USE MY EXERCISE COMPONENT. YOU HAVE THE THINGS NEEDED TO BUILD EXERCISE. */}
+          {/* <h4 className="card-title">{exerciseData.name}</h4>
           <p className="card-text">Sets: {exercise.sets}</p>
           <p className="card-text">Reps: {exercise.reps}</p>
           {exercise.weight && <p className="card-text">Weight: {exercise.weight} lbs</p>}
           {exercise.restTime && <p className="card-text">Rest time: {exercise.restTime} sec</p>}
-          {exercise.notes && <p className="card-text">Notes: {exercise.notes}</p>}
+          {exercise.notes && <p className="card-text">Notes: {exercise.notes}</p>} */}
+          <div className="row">
+          <div className="col-md-6">
+            <Exercise exercise={exerciseData} />
+          </div>
+          <div className="col-md-6">
           <div className="input-group mb-3">
             <span className="input-group-text">Sets performed:</span>
             <input
               type="number"
               className="form-control"
-              onChange={(e) => handleSetReps(exercise.exerciseId, Number(e.target.value), exercise.reps)}
+              onChange={(e) =>
+                handleSetReps(exercise.exerciseId, Number(e.target.value), exercise.reps)
+              }
             />
           </div>
           <div className="input-group mb-3">
@@ -59,11 +92,20 @@ const WorkoutTracker = () => {
             <input
               type="number"
               className="form-control"
-              onChange={(e) => handleSetReps(exercise.exerciseId, exercise.sets, Number(e.target.value))}
+              onChange={(e) =>
+                handleSetReps(exercise.exerciseId, exercise.sets, Number(e.target.value))
+              }
             />
           </div>
-        </div>
-      ))}
+          </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+})}
       <button className="btn btn-primary" onClick={handleSubmit}>Submit Workout</button>
     </div>
   );
