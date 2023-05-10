48850d6640d4989898409e63dddc62b61ce479cd
ayadav <123587824+ayadav313@users.noreply.github.com>
2023-05-10 10:50:33 -0400

    feat(Workout.js): add loading and error states to fetchExercise and fetchExercises functions fix(Workout.js): handle case where workout.exerciseLogs is undefined feat(WorkoutCatalog.js): add loading state to fetchWorkouts function and render loading message while fetching data

--- client/src/components/common/Workout.js
+++ client/src/components/common/Workout.js
@@ -2,30 +2,41 @@ import './Workout.scss';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const fetchExercise = async (exerciseId) => {
-    const response = await fetch("http://localhost:3000/exercises/" + exerciseId)
-    return response.json();
-
-  };
-  
-
 const Workout = ({ workout }) => {
     const [exercises, setExercises] = useState([]);
-  
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
       // Inside the Workout component
       const navigate = useNavigate();
   
       const startWorkout = () => {
         navigate('/workout-tracker', { state: { workoutData: workout } });
       };
+
+      const fetchExercise = async (exerciseId) => {
+        try {
+          const response = await fetch("http://localhost:3000/exercises/" + exerciseId)
+          return response.json();
+        } catch (error) {
+          console.error('Failed to fetch exercises:', error);
+          setError(error.message);
+        }
+
+      };
   
   
-    useEffect(() => {
+      useEffect(() => {
       const fetchExercises = async () => {
-        const fetchedExercises = await Promise.all(
-          workout.exerciseLogs.map(async (log) => await fetchExercise(log.exerciseId))
-        );
-        setExercises(fetchedExercises);
+        if(workout.exerciseLogs)
+        {
+          const fetchedExercises = await Promise.all(
+            workout.exerciseLogs.map(async (log) => await fetchExercise(log.exerciseId))
+          );
+          setExercises(fetchedExercises);
+        }
+        setIsLoading(false);
+
       };
     
       fetchExercises();
@@ -37,24 +48,28 @@ const Workout = ({ workout }) => {
         <div className="card-body">
           <p>Creator: {workout.creator}</p>
           <h5>Exercises:</h5>
-          <ul className="list-unstyled">
-          {exercises.map((exercise, index) => (
-            <li key={index} className="mb-3">
-                <h6>{exercise?.name || 'Exercise name should be here!'}</h6>
-                <p>
-                Sets: <strong>{workout.exerciseLogs[index].sets}</strong>, Reps:{" "}
-                <strong>{workout.exerciseLogs[index].reps}</strong>
-                </p>
-                {workout.exerciseLogs[index].additionalDetails && (
-                <p className="text-muted">
-                    <em>{workout.exerciseLogs[index].additionalDetails}</em>
-                </p>
-                )}
-            </li>
-            ))}
-
-            
-          </ul>
+            {error ? (
+              <p className="error-message">Failed to load exercises: {error}</p>
+              ) : isLoading ? (
+              <p>Loading exercises...</p>
+            ) : (
+              <ul className="list-unstyled">
+                {exercises.map((exercise, index) => (
+                  <li key={index} className="mb-3">
+                      <h6>{exercise?.name || 'Exercise name should be here!'}</h6>
+                      <p>
+                      Sets: <strong>{workout.exerciseLogs[index].sets}</strong>, Reps:{" "}
+                      <strong>{workout.exerciseLogs[index].reps}</strong>
+                      </p>
+                      {workout.exerciseLogs[index].additionalDetails && (
+                      <p className="text-muted">
+                          <em>{workout.exerciseLogs[index].additionalDetails}</em>
+                      </p>
+                      )}
+                  </li>
+                ))}
+              </ul>
+            )}
           <button className="btn btn-primary" onClick={startWorkout}>
     Start
   </button>
--- client/src/components/workoutCatalog/WorkoutCatalog.js
+++ client/src/components/workoutCatalog/WorkoutCatalog.js
@@ -2,66 +2,32 @@ import React, { useState, useEffect } from 'react';
 import './WorkoutCatalog.scss';
 import Workout from '../common/Workout'
 
-
 const WorkoutCatalog = () => {
+
+  //state vars
   const [workouts, setWorkouts] = useState([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchWorkouts = async () => {
-    // Comment out the API request
-    
     try {
       const response = await fetch('http://localhost:3000/workouts');
-      console.log(response);
+      
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
       const data = await response.json();
+      console.log(data);
+
       setWorkouts(data);
+      setIsLoading(false);
+
     } catch (error) {
       console.error('Failed to fetch workouts:', error);
       setError(error.message);
+      setIsLoading(false);
     }
-    
-
-    // Use dummy data
-    const dummyData = [
-      {
-        _id: "1",
-        name: "Workout 1",
-        creator: "John",
-        exerciseLogs: [
-          {
-            exerciseId: "612db8f48a7c18bf22004b0a",
-            sets: 3,
-            reps: 8,
-            additionalDetails: "Rest two minutes between sets",
-          },
-        ],
-      },
-      {
-        _id: "2",
-        name: "Workout 2",
-        creator: "Jane",
-        exerciseLogs: [
-          {
-            exerciseId: "612db8f48a7c18bf22004b0b",
-            sets: 4,
-            reps: 12,
-            additionalDetails: "Rest one minute between sets",
-          },
-          {
-            exerciseId: "612db8f48a7c18bf22004b0b",
-            sets: 4,
-            reps: 12,
-            additionalDetails: "Rest one minute between sets",
-          },
-        ],
-      },
-    ];
-
-    //setWorkouts(dummyData);
   };
 
   useEffect(() => {
@@ -73,6 +39,8 @@ const WorkoutCatalog = () => {
       <h3>Workout Catalog</h3>
       {error ? (
         <p className="error-message">Failed to load workouts: {error}</p>
+      ) : isLoading ? (
+        <p>Loading workouts...</p>
       ) : (
         <div className="workouts-list row row-cols-1 row-cols-md-5 g-4">
           {workouts.map((workout) => (
@@ -87,20 +55,3 @@ const WorkoutCatalog = () => {
 };
 
 export default WorkoutCatalog;
-
-
-// const fetchExercise = async (exerciseId) => {
-//   try {
-//     const response = await fetch(`http://localhost:3000/exercises/${exerciseId}`);
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-
-//     const exerciseData = await response.json();
-//     return exerciseData;
-//   } catch (error) {
-//     console.error(`Failed to fetch exercise ${exerciseId}:`, error);
-//     throw error;
-//   }
-// };
