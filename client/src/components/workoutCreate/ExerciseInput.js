fdbc53764215bde504fb9757745856557f0f5323
ayadav <123587824+ayadav313@users.noreply.github.com>
2023-05-09 21:05:41 -0400

    feat(ExerciseInput.js): refactor ExerciseInput component to use Bootstrap classes feat(WorkoutCreate.js): add form validation and submit functionality to WorkoutCreate component feat(WorkoutCreate.js): add workout name input field to WorkoutCreate component feat(WorkoutCreate.js): add submit and add exercise buttons to WorkoutCreate component

--- client/src/components/workoutCreate/ExerciseInput.js
+++ client/src/components/workoutCreate/ExerciseInput.js
@@ -3,49 +3,51 @@ import './ExerciseInput.scss';
 
 const ExerciseInput = ({ exerciseOptions, exercise, setExercise }) => {
   return (
-    <div className="exercise-input">
-      <select
-        value={exercise.selectedExercise}
-        onChange={(e) => setExercise({ ...exercise, selectedExercise: e.target.value })}
-        className="form-control exercise-input__select"
-      >
-        <option value="">Select an exercise</option>
-        {exerciseOptions.map((option) => (
-          <option key={option._id} value={option._id}>
-            {option.name}
-          </option>
-        ))}
-      </select>
-      <div>
-        <label htmlFor={`sets-${exercise.id}`}>Sets:</label>
-        <input
-          type="number"
-          id={`sets-${exercise.id}`}
-          min="1"
-          value={exercise.sets}
-          onChange={(e) => setExercise({ ...exercise, sets: e.target.value })}
-          className="form-control"
-        />
-      </div>
-      <div>
-        <label htmlFor={`reps-${exercise.id}`}>Reps:</label>
-        <input
-          type="number"
-          id={`reps-${exercise.id}`}
-          min="1"
-          value={exercise.reps}
-          onChange={(e) => setExercise({ ...exercise, reps: e.target.value })}
-          className="form-control"
-        />
-      </div>
-      <div>
-        <label htmlFor={`notes-${exercise.id}`}>Notes:</label>
-        <textarea
-          id={`notes-${exercise.id}`}
-          value={exercise.notes}
-          onChange={(e) => setExercise({ ...exercise, notes: e.target.value })}
-          className="form-control"
-        />
+    <div className="container">
+      <div className="exercise-input">
+        <select
+          value={exercise.selectedExercise}
+          onChange={(e) => setExercise({ ...exercise, selectedExercise: e.target.value })}
+          className="form-select mb-3"
+        >
+          <option value="">Select an exercise</option>
+          {exerciseOptions.map((option) => (
+            <option key={option._id} value={option._id}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+        <div className="mb-3">
+          <label htmlFor={`sets-${exercise.id}`} className="form-label">Sets:</label>
+          <input
+            type="number"
+            id={`sets-${exercise.id}`}
+            min="1"
+            value={exercise.sets}
+            onChange={(e) => setExercise({ ...exercise, sets: e.target.value })}
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor={`reps-${exercise.id}`} className="form-label">Reps:</label>
+          <input
+            type="number"
+            id={`reps-${exercise.id}`}
+            min="1"
+            value={exercise.reps}
+            onChange={(e) => setExercise({ ...exercise, reps: e.target.value })}
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor={`notes-${exercise.id}`} className="form-label">Notes:</label>
+          <textarea
+            id={`notes-${exercise.id}`}
+            value={exercise.notes}
+            onChange={(e) => setExercise({ ...exercise, notes: e.target.value })}
+            className="form-control"
+          />
+        </div>
       </div>
     </div>
   );
